4a7b38b0852ef63efe438cbbe0fc7c35e794e144
BridgetKas <bridgetnamugga@gmail.com>
2025-07-10 13:58:38 +0300

    Fix: Changed the name of the modal component to taskmodal

--- frontend/app/components/modal.tsx
+++ frontend/app/components/modal.tsx
@@ -1,34 +1,25 @@
 
+
 type ModalProps = {
     show:boolean,
     onClose:() => void,
+    children:any
 }
 
 
 
-const Modal = ({show, onClose}:ModalProps) => {
+const TaskModal = ({show, onClose,children}:ModalProps) => {
     return (
       <div className={`modal ${show ? 'modal-show' : ''}`} >
-        <div className="absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-white p-[35px] rounded-[5px] shadow-[0_5px_15px_rgba(0,0,0,0.3)]">
-            <span className="text-black cursor-pointer text-[32px] absolute top-0 right-[15px]" onClick={onClose}>
+        <div className="w-[500px] h-[600px]  overflow-y-auto absolute top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-white px-[35px] pt-[15px] rounded-[5px] shadow-[0_5px_15px_rgba(0,0,0,0.3)]">
+            <span className="text-black cursor-pointer text-[32px] absolute -top-[9px] right-[15px]" onClick={onClose}>
                 &times;
             </span>
             <h1 className="text-[19px] font-bold">Create New Task</h1>
-            <div className="flex items-center gap-2.5 mt-2.5">
-                <button
-                className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors cursor-pointer"
-                >
-                + Create Task
-                </button>
-                <button
-                className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors cursor-pointer"
-                >
-                Cancel
-                </button>
-            </div>
+            {children}
         </div>
       </div>
     );
   };
   
-  export default Modal;
+  export default TaskModal;
