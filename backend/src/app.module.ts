f2a6a6b8c4328a7fdb969c0d037d9d3fb90e5a1b
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-11 16:41:11 +0300

    feat: import chores module in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChoresModule } from './chores/chores.module';
 
 @Module({
-  imports: [],
+  imports: [ChoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
