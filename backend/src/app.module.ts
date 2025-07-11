66cde00effac65724ffed428ec1e2d9f1ebd5c45
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-11 18:00:12 +0300

    refractor: cleaning the app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChoresModule } from './chores/chores.module';
 
 @Module({
   imports: [ChoresModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
