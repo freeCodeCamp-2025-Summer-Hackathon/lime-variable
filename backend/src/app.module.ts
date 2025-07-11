2e0c15f596f7878d063349836be66812bd05263b
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-11 18:01:31 +0300

    feat: instantiate prisma module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChoresModule } from './chores/chores.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ChoresModule],
+  imports: [ChoresModule, PrismaModule],
 })
 export class AppModule {}
