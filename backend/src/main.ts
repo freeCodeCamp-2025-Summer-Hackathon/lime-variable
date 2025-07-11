5b0a7376129586a6f1c4c6c67b87960180ebd300
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-07-11 16:53:40 +0300

    feat: add dto validation in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -13,7 +14,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
